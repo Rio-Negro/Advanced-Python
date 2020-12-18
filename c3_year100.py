24f0dd2f15bc31442cf9c683383b836dc9efaf36
Hasani <76195183+Rio-Negro@users.noreply.github.com>
2020-12-18 00:59:40 -0500

    Add files via upload

---
+++ c3_year100.py
@@ -0,0 +1,14 @@
+
+def centeganarian(age) :
+    year_100 = 2020 - age + 100
+    return year_100
+
+name = input("Hello what's your name:")
+age = int(input("How old are you?"))
+print("you will be 100 in: {}".format(centeganarian(age)))
+
+
+
+
+
+
